781f3d809442fca2697d27845fe054d0e79f36b4
HuZeJi <69708350+HuZeJi@users.noreply.github.com>
2021-06-12 22:43:01 -0600

    Cambios relacionados al sprint 1 y 2

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-/*server.js*/
+"use strict";
 
-const http = require('http');
+const dotenv = require("dotenv");
+dotenv.config({ path: "environments/.env", encoding: "utf-8" });
 
-const hostname = '127.0.0.1';
-const port = 3000;
+var express = require("express");
+var app = express();
+var log4js = require("log4js");
+const routes = require("./routes/routes");
+var bodyParser = require("body-parser");
+const mc = require("./dao/mongoClient");
 
-const server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+log4js.configure({
+    appenders: {
+        console: {
+            type: "console",
+        },
+    },
+    categories: {
+        default: {
+            appenders: ["console"],
+            level: "info",
+        },
+    },
 });
 
-server.listen(port, hostname, function() {
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
-});
+async function loadRoutes() {
+    routes.assignRoutes(app);
+}
+
+module.exports = app;
+
+try {
+    var logger = log4js.getLogger("ex-afja-api/index.js");
+    app.use(bodyParser.urlencoded({ limit: '5000mb', extended: true, parameterLimit: 50000 }));
+    app.use(bodyParser.json({ limit: '5000mb', extended: true }));
+    loadRoutes();
+
+    app.timeout = 120000;
+} catch (error) {
+    logger.error(error);
+}
