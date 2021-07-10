24a7dfe6b1031dfc69a9cad2ebc46d310ca2ae35
HuZeJi <69708350+HuZeJi@users.noreply.github.com>
2021-07-09 22:10:44 -0600

    FIX Cambios a variables de entorno

--- dao/mongoClient.js
+++ dao/mongoClient.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = process.env._MONGO_URL;
-const dbName = process.env._MONGO_DB_NAME;
+const url = process.env.MONGO_URL;
+const dbName = process.env.MONGO_DB_NAME;
 const client = new MongoClient(url);
 
 const mongoose = require("mongoose");
